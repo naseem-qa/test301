9188b0d2f213621e01a30264c6b84a6a4cafe94c
naseem <izzatnaseem30@gmail.com>
2020-07-03 16:17:57 +0300

    without local storage

--- app.js
+++ app.js
@@ -5,26 +5,24 @@ var container = document.getElementById('table');
 var table = document.createElement('table');
 container.appendChild(table);
 
-var carsArr = ['BMW','Mercedes','Cadillac'];
-
 function Cars(carModel, modelYear, manufacturer) {
     this.carModel = carModel;
     this.modelYear = modelYear;
     this.manufacturer = manufacturer;
     this.price;
+    Cars.all.push(this)
 }
 
-// var BMW = new Cars('BMW', 1980,'Germany');
-// var Mercedes = new Cars('Mercedes', 1990, 'Germany');
-// var Cadillac = new Cars('Cadillac', 1990, 'USA');
-var vehicles =[];
-vehicles.push(new Cars('BMW', 1980,'Germany'));
-vehicles.push(new Cars('Mercedes', 1990, 'Germany'));
-vehicles.push(new Cars('Cadillac', 1990, 'USA'));
+Cars.all =[];
+new Cars('BMW', 1980,'Germany');
+new Cars('Mercedes', 1990, 'Germany');
+new Cars('Cadillac', 1990, 'USA');
+console.log('ARR', Cars.all)
 
-Cars.prototype.randomPrice = function () {
+const randomPrice = function (car) {
         var carRandomPrice = Math.ceil(Math.floor(Math.random() * (100000 - 7000)) + 7000);
-        this.price = carRandomPrice;
+        console.log('car',car)
+        car.price = carRandomPrice;
 }
 
 
@@ -48,50 +46,74 @@ function renderHr(table) {
         tr.appendChild(th);
         th.textContent = 'Car Manufacturer';
 }
-renderHr(table);
 
 
-Cars.prototype.render = function (table) {
+const render = function (table) {
 
-    var tr = document.createElement('tr');
-    table.appendChild(tr);
+    for (let i = 0; i < Cars.all.length; i++) {
 
-    var td = document.createElement('td');
-    tr.appendChild(td);
-    td.textContent = this.carModel;
-    
-    var td = document.createElement('td');
-    tr.appendChild(td);
-    td.textContent = this. modelYear;
+        console.log('love', Cars.all)
+        
+        randomPrice(Cars.all[i]);
 
-    var td = document.createElement('td');
-    tr.appendChild(td);
-    td.textContent = this.price;
-
-    var td = document.createElement('td');
-    tr.appendChild(td);
-    td.textContent = this.manufacturer;
+        var tr = document.createElement('tr');
+        table.appendChild(tr);
+        
+        var td = document.createElement('td');
+        tr.appendChild(td);
+        td.textContent = Cars.all[i].carModel;
+        
+        var td = document.createElement('td');
+        tr.appendChild(td);
+        td.textContent = Cars.all[i]. modelYear;
+        
+        var td = document.createElement('td');
+        tr.appendChild(td);
+        td.textContent = Cars.all[i].price;
+        
+        var td = document.createElement('td');
+        tr.appendChild(td);
+        td.textContent = Cars.all[i].manufacturer;
+    }
 }
 
-for (let index = 0; index < vehicles.length; index++) {
-    vehicles[index].randomPrice();
-    vehicles[index].render(table);
-}
+
 
 function formNew(event) {
     event.preventDefault();
+    table.innerHTML = '';
 
     var carModel = event.target.carModel.value;
     var modelYear = parseInt(event.target.modelYear.value);
-    var manufacturer =event.target.manufacturer.value;
+    var manufacturer = event.target.manufacturer.value;
+    new Cars(carModel, modelYear, manufacturer);
+    console.log('here', Cars.all);
+    // Cars.all.push(newCar);
+    renderHr(table);
+    render(table);
+    setData();
 
-    var newCar = new Cars(carModel, modelYear, manufacturer);
-    vehicles.push(newCar);
-    carsArr.push(carModel);
+}
 
-    newCar.randomPrice();
-    newCar.render(table);
+//     var vehiclesString = localStorage.getItem('newVehicles');  
+//       var rawObjectArray = JSON.parse(vehiclesString);
+function setData() {   
+    var vehiclesArr = JSON.stringify(Cars.all);
+    localStorage.setItem('newVehicles', vehiclesArr);
+}
+
+
+function getData() {
+    let storedCars = localStorage.getItem('newVehicles');
+    console.log('storedCars',JSON.parse(storedCars));
+    if (storedCars) {
+        Cars.all = JSON.parse(storedCars);  
+    }
 }
 
 var form = document.getElementById('carsForm');
 form.addEventListener('submit', formNew);
+
+getData();
+renderHr(table);
+render(table);
